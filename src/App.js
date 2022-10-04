389b20ea43c70b94497dcda65769ce5af29962ae
Abhilash Sethi <89596373+abhilashsethi@users.noreply.github.com>
2022-10-04 07:05:31 +0530

    stack navigation

--- src/App.js
+++ src/App.js
@@ -1,176 +1,77 @@
 import React, {useState} from 'react';
 import {
+  SafeAreaView,
   StyleSheet,
   View,
   Text,
-  TextInput,
-  Pressable,
-  Alert,
-  ToastAndroid,
-  Modal,
-  Image,
-  ImageBackground,
+  TouchableOpacity,
 } from 'react-native';
-import CustomButton from './CustomButton';
-import Header from './Header';
+// import {NavigationContainer} from '@react-navigation/native';
+// import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
+// const App = () => {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen name="Home" component={Home} />
+//         <Stack.Screen name="Main" component={Main} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// };
+// const Stack = createNativeStackNavigator();
 const App = () => {
-  const [name, SetName] = useState('');
-  const [submitted, SetSubmitted] = useState(false);
-  const [showWarning, SetshowWarning] = useState(false);
-  const onPressHandler = () => {
-    if (name.length > 3) {
-      SetSubmitted(!submitted);
-    } else {
-      SetshowWarning(true);
-    }
-  };
-
   return (
-    <ImageBackground
-      style={styles.body}
-      source={{
-        uri: 'https://cdn.pixabay.com/photo/2013/07/12/12/35/texture-145968_960_720.png',
+    <SafeAreaView
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
       }}>
-      <Header />
-      <Modal
-        visible={showWarning}
-        transparent
-        onRequestClose={() => SetshowWarning(false)}
-        animationType="slide"
-        hardwareAccelerated>
-        <View style={styles.centered_view}>
-          <View style={styles.warning_modal}>
-            <View style={styles.warning_title}>
-              <Text style={styles.text}>WARNING!</Text>
-            </View>
-            <View style={styles.warning_body}>
-              <Text style={styles.text}>
-                The name must be longer than 3 charachters
-              </Text>
-            </View>
-            <Pressable
-              onPress={() => SetshowWarning(false)}
-              style={styles.warning_button}
-              android_ripple={{color: '#fff'}}>
-              <Text style={styles.text}>OK</Text>
-            </Pressable>
-          </View>
-        </View>
-      </Modal>
-      <Text style={styles.text}>Please write your name:</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="e.g. John"
-        onChangeText={value => SetName(value)}
-      />
-      <CustomButton
-        onPressFunction={onPressHandler}
-        title={submitted ? 'Clear' : 'Submit'}
-        color={'#00ff00'}
-      />
-      <CustomButton
-        onPressFunction={() => {}}
-        title={'Test'}
-        color={'#ff00ff'}
-        style={{margin: 10}}
-      />
-      {/* <Pressable
-        onPress={onPressHandler}
-        hitSlop={{ top: 10, bottom: 10, right: 10, left: 10 }}
-        android_ripple={{ color: '#00f' }}
-        style={({ pressed }) => [
-          { backgroundColor: pressed ? '#dddddd' : '#00ff00' },
-          styles.button
-        ]}
-      >
-        <Text style={styles.text}>
-          {submitted ? 'Clear' : 'Submit'}
+      <View>
+        <Text style={styles.text}>GameOn</Text>
+      </View>
+      <TouchableOpacity
+        style={{
+          backgroundColor: '#ad40af',
+          padding: 20,
+          width: '90%',
+          borderRadius: 5,
+          alignItems: 'center',
+        }}>
+        <Text style={{fontWeight: 'bold', fontSize: 18, color: '#fff'}}>
+          Let's Begin
         </Text>
-      </Pressable> */}
-      {submitted ? (
-        <View style={styles.body}>
-          <Text style={styles.text}>You are registered as {name}</Text>
-          <Image
-            style={styles.image}
-            source={require('../assets/done.png')}
-            resizeMode="stretch"
-          />
-        </View>
-      ) : (
-        <Image
-          style={styles.image}
-          source={{
-            uri: 'https://cdn.pixabay.com/photo/2018/01/04/15/51/404-error-3060993_960_720.png',
-          }}
-          resizeMode="stretch"
-        />
-      )}
-    </ImageBackground>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
+};
+
+const Home = () => {
+  <View
+    style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'white',
+    }}>
+    <Text>Home Screen</Text>
+  </View>;
 };
 
 const styles = StyleSheet.create({
   body: {
     flex: 1,
+    backgroundColor: 'white',
     alignItems: 'center',
+    justifyContent: 'center',
   },
   text: {
-    color: '#000000',
-    fontSize: 20,
-    margin: 10,
-    textAlign: 'center',
-  },
-  input: {
-    width: 200,
-    borderWidth: 1,
-    borderColor: '#555',
-    borderRadius: 5,
-    textAlign: 'center',
-    fontSize: 20,
-    marginBottom: 10,
-  },
-  button: {
-    width: 150,
-    height: 50,
-    alignItems: 'center',
-  },
-  centered_view: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#00000099',
-  },
-  warning_modal: {
-    width: 300,
-    height: 300,
-    backgroundColor: '#ffffff',
-    borderWidth: 1,
-    borderColor: '#000',
-    borderRadius: 20,
-  },
-  warning_title: {
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ff0',
-    borderTopRightRadius: 20,
-    borderTopLeftRadius: 20,
-  },
-  warning_body: {
-    height: 200,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  warning_button: {
-    backgroundColor: '#00ffff',
-    borderBottomLeftRadius: 20,
-    borderBottomRightRadius: 20,
-  },
-  image: {
-    width: 100,
-    height: 100,
-    margin: 10,
+    color: '#20315f',
+    fontSize: 30,
+    fontStyle: 'italic',
+    fontWeight: 'bold',
   },
 });
 
---
+++ src/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Home = () => {
+  <View
+    style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'white',
+    }}>
+    <Text
+      style={{
+        color: '#20315f',
+        fontSize: 30,
+        fontStyle: 'italic',
+        fontWeight: 'bold',
+      }}>
+      Home Screen
+    </Text>
+  </View>;
+};
+export default Home;
